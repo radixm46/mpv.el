516528d405366852edaabf491ab8398eaf0305ed
radixm46 <radixm46@gmail.com>
2025-12-07 09:59:24 +0900

    Clean up for readability
    Co-authored-by: Johann Klähn <johann@jklaehn.de>

--- mpv.el
+++ mpv.el
@@ -487,19 +487,17 @@ See `mpv-start' if you need to pass further arguments and
   "Append THING to the current mpv playlist.
 
 If ARGS are provided, they are passed as per-file options to mpv."
-  (let* ((req-idx (version<= "0.38"
-                             (replace-regexp-in-string
-                              "[^0-9.]" "" (mpv-get-property "mpv-version"))))
-         ;; mpv above 0.38.0 requires insert position
-         (opts-args (append '("append")
-                            (when req-idx '("-1"))
-                            (list
-                             (string-join
-                              (mapcar (lambda (arg)
-                                        (string-trim-left arg "--"))
-                                      args)
-                              ",")))))
-    (apply #'mpv-run-command "loadfile" thing opts-args))
+  (let ((mpv-version (replace-regexp-in-string
+                      "[^0-9.]" "" (mpv-get-property "mpv-version"))))
+    (apply #'mpv-run-command "loadfile" thing
+           `("append"
+             ;; mpv above 0.38.0 requires insert position argument
+             ,@(when (version<= "0.38" mpv-version) '("-1"))
+             ,(string-join
+               (mapcar (lambda (arg)
+                         (string-trim-left arg "--"))
+                       args)
+               ","))))
   (when-let* ((count (mpv-get-property "playlist-count"))
               (index (1- count))
               (filename (mpv-get-property (format "playlist/%d/filename" index))))
